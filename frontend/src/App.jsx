1c9af4de63ae7c7c50f6df266b1a2ea5bc5340fc
Sebin Mathew <sebinmathew543@gmail.com>
2026-06-22 19:25:07 +0530

    chore: remove unused PharmacyDashboard prototype and cookie-parser dependency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import FirstPage from './components/FirstPage.jsx';
 import MapPage from './components/MapPage.jsx';
 import PharmacyAdmin from './components/PharmacyAdmin.jsx';
 import PharmacyPage from './components/PharmacyPage.jsx';
-import PharmacyDashboard from './components/PharmacyDashboard.jsx';
 import LoginPage from './components/LoginPage.jsx';
 import SignupPage from './components/SignupPage.jsx';
 
@@ -16,7 +15,6 @@ function App() {
         <Route path="/mappage" element={<MapPage />} />
         <Route path="/pharmacy" element={<PharmacyPage />} />
         <Route path="/pharmacy/admin" element={<PharmacyAdmin />} />
-        <Route path="/pharmacy/dashboard" element={<PharmacyDashboard />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
